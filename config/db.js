805218f530d53aea10224aea5f38fe758dce0537
sathishakarapu <akarapusathish003@gmail.com>
2024-07-22 18:23:53 +0530

    changes done

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 // const uri = "mongodb+srv://contactmdb:4VNC0SSbLclk9m27@cluster0.aiqgimm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/contact_manager"
 // const uri = "mongodb+srv://sathish:Swathi%40123@cluster0.fygopsn.mongodb.net";
-const uri = process.env.URI;
+const uri = "mongodb+srv://sathish:Swathi%40123@cluster0.fygopsn.mongodb.net";
 const connectDB = async () => {
   return mongoose
     .connect(`${uri}/contact-manager`)
